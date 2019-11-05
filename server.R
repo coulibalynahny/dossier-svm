e0da911d6f53e3e69277e770ee5e45810eef5c5b
coulibalynahny <coulibalynahny@yahoo.fr>
2019-11-05 09:54:25 +0100

    dossier

---
+++ server.R
@@ -0,0 +1,285 @@
+# SERVER
+
+library(shiny)
+library(mlr)
+library(rpart)
+library(rpart.plot)
+library(e1071)
+library(caret)
+library(DMwR)
+library(FSelectorRcpp)
+library(randomForest)
+library(data.table)
+library(DT)
+
+DT=read.csv("C:/Users/nahny/OneDrive/Bureau/master ESA/master 2 ESA/SVM/creditcard.csv", stringsAsFactors = FALSE )
+
+
+DT$Amount=as.vector(scale(DT$Amount))
+DT$Time=as.vector(scale(DT$Time))
+DT$Class=as.factor(DT$Class)
+levels(DT$Class)[1] <- "nonFraud"
+levels(DT$Class)[2] <- "Fraud"
+table_prop<-rbind(table(DT$Class),prop.table(table(DT$Class)))
+row.names(table_prop)<-c("Number","Proportion (in %)")
+table_prop[2,]<-round(table_prop[2,],4)*100
+tableex=DT[1:100,c(1,2,3,4,5,6,30,31)]
+stat=summary(DT[,-31])
+
+
+set.seed(1234)
+inTrain = createDataPartition(DT$Class, p = 0.7,list=FALSE)
+data.train = as.data.frame(DT[inTrain,])  
+data.test = as.data.frame(DT[-inTrain,])
+
+
+shinyServer(function(input, output) {
+    
+    # on reequilibre l'echantillon train avec smote, on definit un echantillon nomé data.smote
+    data.smote <- reactive({ 
+        as.data.frame(SMOTE(Class ~., data.train, perc.over = input$over, k = input$ksmote,
+                            perc.under = input$under))
+        
+    }) 
+    # table dt  
+    #output$tableDT1 <- renderPrint({
+       # table(DT$Class)
+    #})
+    #output$tableDT <- renderPrint({
+        #prop.table(table(DT$Class))
+    #})
+    
+
+    output$tableDT2<-renderDT({
+            datatable(table_prop,list(dom = 't'))
+    })
+    
+    output$tableDTt<-renderDT({
+        datatable(tableex)
+    })
+    
+    output$tableDTs<-renderDT({
+        datatable(stat)
+    })
+    
+    # table data.train
+    output$tabletrain1 <- renderPrint({
+        table(data.train$Class)
+    })
+    output$tabletrain <- renderPrint({
+        prop.table(table(data.train$Class))
+    })
+    # table data.smote
+    output$tableSmote1 <- renderPrint({
+        table(data.smote()$Class)
+    })
+    output$tableSmote <- renderPrint({
+        prop.table(table(data.smote()$Class))
+    })
+    
+    ###############################################################################
+    # MODELISATION
+    ###############################################################################
+    
+    
+    
+    # dans le package mlr on definit une tache qui permet de specifier une fois pour toute le probleme que nous traitons
+    #cest a dire la base de donee utilisee, la variable cible et la modalite cible
+    train.task <- reactive({
+        makeClassifTask(data=data.smote(),target="Class",positive = "Fraud")
+    })
+    
+    test.task <- reactive({
+        makeClassifTask(data=data.test,target="Class",positive = "Fraud")
+    })
+    
+    filter.kruskal <- reactive({
+        mlr::generateFilterValuesData(task=train.task(),method="FSelectorRcpp_information.gain")
+    })
+    
+    output$filtre <- renderPlot({ mlr::plotFilterValues(filter.kruskal(),n.show=10)
+    }) 
+    
+    #arbre de decision 
+    train.tree <- reactive({ 
+        
+        learner.tree <- makeLearner(cl="classif.rpart",predict.type="prob")
+        
+        train.tree <- mlr::train(learner.tree,train.task())
+        
+        return(train.tree)
+    }) 
+    
+    
+    output$tree <- renderPlot({
+        
+        rpart.plot(getLearnerModel(train.tree()),roundint = F)
+        
+    })
+    
+    
+    pred.tree <- reactive({ 
+        predict(train.tree(),test.task())
+    }) 
+    
+    
+    output$matrix.tree <- renderPrint({
+        calculateConfusionMatrix(pred.tree(),relative = TRUE)
+    })
+    
+    
+    #svm 
+    train.svm <- reactive({ 
+        
+        learner.svm <- makeLearner(cl="classif.svm",predict.type="prob",
+                                   par.vals = list(cost = input$cost.param,
+                                                   kernel = input$kernel.param))
+        
+        train.svm <- mlr::train(learner.svm,train.task())
+        
+        return(train.svm)
+    }) 
+    
+    
+    pred.svm <- reactive({ 
+        predict(train.svm(),test.task())
+    }) 
+    
+    
+    output$matrix.svm <- renderPrint({
+        calculateConfusionMatrix(pred.svm(),relative = TRUE)
+    })
+    
+    output$roc.svm <- renderPrint({
+        calculateROCMeasures(pred.svm())
+    })
+    
+    
+    df <- reactive({ generateThreshVsPerfData(pred.svm(), measures = list(fpr, tpr, ppv, tnr,mmce))
+    })
+    
+    
+    output$graph1.svm <- renderPlot({
+         #plotROCCurves(df(), measures = list(tpr, ppv), diagonal = FALSE)
+         ggplot(df()$data)+geom_line(aes(x=tpr,y=ppv),color="blue",size=0.6)+theme_minimal()+
+        xlab("True positive rate")+ylab("Positive predict rate")
+     })
+    
+#     output$graph1.svm <- renderPlot({
+#        print(df()$data[,c("tpr")])
+#        print(base::rep(df()$data[,c("tpr")],times=2))
+#         data_2<-data.frame(tpr=base::rep(df()$data[,c("tpr")],times=2),
+#                                  values=c(df()$data[,c("tnr")],df()$data[,c("fpr")]),
+#                                  group=c(base::rep("graph 1",dim(df()$data[,c("tpr")])[1]),base::rep("graph 2",dim(df()$data[,c("tpr")])[1])))
+# print(data_2)
+# print(df()$data[,c("tpr")])
+#         plotROCCurves(df(), measures = list(tpr, ppv), diagonal = FALSE)
+#         ggplot(data_2)+geom_line(aes(x=tpr,y=values,group=group),color="blue",size=3)+theme_minimal()+
+#              xlab("True positive rate")+ylab("Positive predict rate")
+#     })
+    
+    output$graph2.svm <- renderPlot({
+        # plotROCCurves(df(), measures = list(tnr, tpr), diagonal = FALSE)
+        ggplot(df()$data)+geom_line(aes(x=tnr,y=tpr),color="orange",size=0.6)+theme_minimal()+
+            xlab("True negative rate")+ylab("True positive rate")
+    })
+    
+     output$graph3.svm <- renderPlot({
+    #     plotROCCurves(df(), measures = list(fpr, tpr), diagonal = TRUE)
+         ggplot(df()$data)+geom_line(aes(x=fpr,y=tpr),color="green",size=0.6)+theme_minimal()+
+             xlab("false positive rate")+ylab("True positive rate")
+    })
+    
+    
+    
+    
+    
+    
+    
+    
+    #random forest
+    train.rf <- reactive({ 
+        
+        learner.rf <- makeLearner(cl="classif.randomForest",predict.type="prob",
+                                  par.vals = list(ntree = input$ntree.param,
+                                                  mtry = input$mtry.param))
+        
+        train.rf <- mlr::train(learner.rf,train.task())
+        
+        return(train.rf)
+    }) 
+    
+    
+    pred.rf <- reactive({ 
+        predict(train.rf(),test.task())
+    }) 
+    
+    
+    output$matrix.rf <- renderPrint({
+        calculateConfusionMatrix(pred.rf(),relative = TRUE)
+    })
+    
+    output$roc.rf <- renderPrint({
+        calculateROCMeasures(pred.rf())
+    })
+    
+    
+    df.rf <- reactive({ generateThreshVsPerfData(pred.rf(),
+                                                 measures = list(fpr, tpr, ppv, tnr,mmce))
+    })
+    
+    
+    output$graph1.rf <- renderPlot({
+        plotROCCurves(df.rf(), measures = list(tpr, ppv), diagonal = FALSE)
+    })
+    
+    output$graph2.rf <- renderPlot({
+        plotROCCurves(df.rf(), measures = list(tnr, tpr), diagonal = FALSE)
+    })
+    
+    output$graph3.rf <- renderPlot({
+        plotROCCurves(df.rf(), measures = list(fpr, tpr), diagonal = TRUE)
+    })
+    
+    
+    # benchmarking
+    rdesc=reactive({ makeResampleDesc("CV", iters = input$k, stratify = TRUE)
+    })
+    
+    bmr=reactive({ 
+        ms = list(mlr::auc, mmce)
+        lrns = list(makeLearner("classif.svm", predict.type = "prob"),
+                    makeLearner("classif.rpart", predict.type = "prob"),
+                    makeLearner("classif.randomForest", predict.type = "prob"),
+                    makeLearner("classif.logreg", predict.type = "prob")
+        )
+        bmr = benchmark(lrns, train.task(), rdesc(), measures = ms, models = TRUE)
+        return(bmr)
+    })
+    
+    output$bmr1 <- renderPrint({
+        bmr()
+    })
+    
+    df.bmr <- reactive({ generateThreshVsPerfData(bmr(),
+                                                  measures = list(fpr, tpr, ppv, tnr,mmce))
+    })
+    
+    
+    output$graph1.bmr <- renderPlot({
+        plotROCCurves(df.bmr(), measures = list(tpr, ppv), diagonal = FALSE)
+    })
+    
+    output$graph2.bmr <- renderPlot({
+        plotROCCurves(df.bmr(), measures = list(tnr, tpr), diagonal = FALSE)
+    })
+    
+    output$graph3.bmr <- renderPlot({
+        plotROCCurves(df.bmr(), measures = list(fpr, tpr), diagonal = TRUE)
+    })
+    
+    output$bmrplot <- renderPlot({
+        plotBMRBoxplots(bmr(), measure = mmce, order.lrn = getBMRLearnerIds(bmr()))
+    })
+    
+})
---
+++ ui.R
@@ -0,0 +1,153 @@
+# UI
+
+library(shiny)
+library(shinythemes)
+
+# Define UI for application that draws a histogram
+shinyUI(fluidPage(
+    shinythemes::themeSelector(),
+    
+    titlePanel("SVM project on R Shiny"),
+    actionButton("download","download"),
+    
+        h2("I.data visualisation",
+           h4("our database contains the list of clients of a bank. 
+              Customers can be categorized into two groups, which are customers in fraud situations and non-fraud customers."),
+           h3("1.first view of the database"),
+           dataTableOutput("tableDTt"),
+           h3("2.some descriptive statistics"),
+           dataTableOutput("tableDTs")
+       
+        ),
+        
+        h2("II.rebalance database",
+                 
+                 h3("Class distribution in the Original database"),
+                 #verbatimTextOutput("tableDT1"),
+                 #verbatimTextOutput("tableDT"),
+                 dataTableOutput("tableDT2"),
+                 h4("our database contains a total of 284808 observations including 
+              492 observations for individuals in a fraud situation and the rest for individuals in non-fraud situations. Which allows us to notice that our database is not balanced. 
+              We have 99.83% of non fraud and 0.17% of fraud. before any modeling, we will rebalance the database to implement the calculations more easily"),
+                 
+                 h3("Distribution of Class in the training database"),
+                 verbatimTextOutput("tabletrain1"),
+                 verbatimTextOutput("tabletrain"),
+                 
+                 h3("Rebalancing training database using Smote"),
+                 
+                 sliderInput("ksmote","number of nearest neighbours that are used 
+                             to generate the new examples of fraud",
+                             3,10,value=5),
+                 
+                 sliderInput("over","oversampling fraud case",
+                             100,1000,value=500,round = T),
+                 
+                 sliderInput("under","undersampling non fraud case",
+                             50,100,value=80,round = T),
+                 
+                 h4("Distribution of Class in the Smote data sets"),
+                 verbatimTextOutput("tableSmote1"),
+                 verbatimTextOutput("tableSmote"),
+                 plotOutput("filtre")
+                 
+                 
+        ),
+                h2("III.the models"),
+    
+    navlistPanel(
+        "list of models to test",
+        
+        #svm
+        tabPanel("SVM",
+                 h1("SVM model"),
+                 
+                 h3("hyperparameters's choise"),
+                 
+                 sliderInput("cost.param","choice of cost parameters",
+                             0,10,value=1,round = T),
+                 
+                 selectInput("kernel.param", "choice of kernel", 
+                             choices= c("linear","sigmoid","polynomial","radial")),
+                 
+                 h4("confusion matrix"),
+                 verbatimTextOutput("matrix.svm"),
+                 
+                 h4("ROC measures"),
+                 verbatimTextOutput("roc.svm"),
+                 
+                 h4("Precision/recall graph"),
+                 plotOutput("graph1.svm"),
+                 
+                 h4("Sensitivity/specificity graph"),
+                 plotOutput("graph2.svm"),
+                 
+                 h4("roc curve"),
+                 plotOutput("graph3.svm")
+                 
+        ),
+        
+        # decision tree
+        tabPanel("decision tree",
+                 h1("decision tree"),
+                 h5("construction of the decision tree"),
+                 plotOutput("tree"),
+                 h3("the result analysis"),
+                 h4("confusion matrix"),
+                 verbatimTextOutput("matrix.tree")
+                 
+        ),
+        #random forest
+        tabPanel("random forest",
+                 h2("random forest model"),
+                 
+                 h3("choice of hyperparameters"),
+                 
+                 sliderInput("ntree.param","Number of tree",
+                             100,500,value=200,round = T),
+                 
+                 sliderInput("mtry.param", "Number of variables randomly sampled as candidates at each split", 
+                             3,8,value=5),
+                 
+                 h4("confusion matrix"),
+                 verbatimTextOutput("matrix.rf"),
+                 
+                 h4("ROC measures"),
+                 verbatimTextOutput("roc.rf"),
+                 
+                 h4("Precision/recall graph"),
+                 plotOutput("graph1.rf"),
+                 
+                 h4("Sensitivity/specificity graph"),
+                 plotOutput("graph2.rf"),
+                 
+                 h4("roc curve"),
+                 plotOutput("graph3.rf")
+                 
+        ),
+        
+        # model comparision
+        tabPanel("model comparison",
+                 h2("comparison of different models"),
+                 h5("We chose to re-sample the model in two ways that are: k-fold and holdout. "),
+                 h4("resampling using k-fold method"),
+                 sliderInput("k","chose your K ",0,10,value=5,round = T),
+                 verbatimTextOutput("bmr1"),
+                 
+                 h4("Precision/recall graph"),
+                 plotOutput("graph1.bmr"),
+                 
+                 h4("Sensitivity/specificity graph"),
+                 plotOutput("graph2.bmr"),
+                 
+                 h4("roc curve"),
+                 plotOutput("graph3.bmr"),
+                 
+                 h4("distribution of performance values across resampling iterations for
+                    one performance measure and for all learners"),
+                 plotOutput("bmrplot")
+                 
+        )
+        
+        
+    )))
