f296041f05fe1773cc754b128ce5fbd06ac654f0
Todirca <teodora.todirca94@yahoo.com>
2019-11-18 00:42:28 +0100

    Mise à jour Données

--- server.R
+++ server.R
@@ -16,9 +16,9 @@ library(plotly)
 
 # chemin d'acces C:\Users\nahny\Documents\GitHub
 
-DT=read.csv("C:/Users/nahny/Documents/GitHub/creditcard.csv", stringsAsFactors = FALSE )
+#DT=read.csv("C:/Users/nahny/Documents/GitHub/creditcard.csv", stringsAsFactors = FALSE )
 
-#DT=read.csv("C:/Users/pierr/Desktop/svm/dossier-svm/creditcard.csv", stringsAsFactors = FALSE )
+DT=readRDS("templatePDF/creditcard.rds")
 
 #DT=read.csv("C:/Users/33668/Documents/MASTER 2 ESA/projet svm/app_0611/creditcard.csv", stringsAsFactors = FALSE )
 
